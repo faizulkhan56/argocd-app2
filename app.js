f5a2e1647b6fe70d84a5061be02d20429e0854b5
Ubuntu <faizulkhan56@gmail.com>
2025-05-26 08:48:10 +0000

    app.js changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from argo-app-test2');
+  res.send('Hello from argo-app-test2 new test');
 });
 
 const PORT = 3003;
