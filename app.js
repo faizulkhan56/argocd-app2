6d97895c07d2a9772b208fef2ec795fbd49cd189
Ubuntu <faizulkhan56@gmail.com>
2025-05-26 07:27:47 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from argo-app-test2');
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
